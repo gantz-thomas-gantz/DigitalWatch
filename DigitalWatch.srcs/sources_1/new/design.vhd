965dfcfc78f079356c6264101e79ece5a8a50a48
ThomasG <thomas.gantz@stud.uni-heidelberg.de>
2025-11-12 14:14:05 +0100

    minimal working (?) version

--- DigitalWatch.srcs/sources_1/new/design.vhd
+++ DigitalWatch.srcs/sources_1/new/design.vhd
@@ -4,11 +4,11 @@ use ieee.numeric_std.all;
 
 entity top_watch is
   port (
-    clk        : in  std_logic;
+    clk        : in  std_logic;     -- system clock
     rst        : in  std_logic;
     btn_reset  : in  std_logic;
     btn_mode   : in  std_logic;
-    btn_select : in std_logic;
+    btn_select : in  std_logic;
     btn_inc    : in  std_logic;
 
     HH : out unsigned(4 downto 0);
@@ -17,9 +17,9 @@ entity top_watch is
 
     alarm_active : out std_logic
   );
-end entity top_watch;
+end entity;
 
-architecture top_watch_architecture of top_watch is
+architecture rtl of top_watch is
 
   --------------------------------------------------------------------
   -- State machine definition
@@ -33,78 +33,164 @@ architecture top_watch_architecture of top_watch is
     SET_TIME_MM,
     SET_TIME_SS,
     SET_ALARM_HH,
-    SET_ALARM_MM,
+    SET_ALARM_MM
   );
 
   signal state : state_t := IDLE;
 
+  signal hh_reg : unsigned(4 downto 0) := (others => '0');
+  signal mm_reg : unsigned(5 downto 0) := (others => '0');
+  signal ss_reg : unsigned(5 downto 0) := (others => '0');
+  signal alarm_active_reg : std_logic := '0';
+
+  -- 1 Hz tick generator
+  constant CLK_FREQ : natural := 50_000_000;  -- adjust to board clock
+  signal tick_cnt   : unsigned(25 downto 0) := (others => '0');
+  signal tick_1s    : std_logic := '0';
+
 begin
 
+  HH <= hh_reg;
+  MM <= mm_reg;
+  SS <= ss_reg;
+  alarm_active <= alarm_active_reg;
+
   --------------------------------------------------------------------
-  -- MAIN FSM
+  -- 1 Hz tick generator
   --------------------------------------------------------------------
   process(clk, rst)
   begin
     if rst = '1' then
-      cur_state <= IDLE;
-
+      tick_cnt <= (others => '0');
+      tick_1s  <= '0';
     elsif rising_edge(clk) then
-      case cur_state is
+      if tick_cnt = to_unsigned(CLK_FREQ - 1, tick_cnt'length) then
+        tick_cnt <= (others => '0');
+        tick_1s  <= '1';
+      else
+        tick_cnt <= tick_cnt + 1;
+        tick_1s  <= '0';
+      end if;
+    end if;
+  end process;
 
+  --------------------------------------------------------------------
+  -- Timekeeping mechanism
+  --------------------------------------------------------------------
+  process(clk, rst)
+  begin
+    if rst = '1' then
+      ss_reg <= (others => '0');
+      mm_reg <= (others => '0');
+      hh_reg <= (others => '0');
+    elsif rising_edge(clk) then
+      if tick_1s = '1' then
+        -- increment time ALWAYS
+        if ss_reg = 59 then
+          ss_reg <= (others => '0');
+          if mm_reg = 59 then
+            mm_reg <= (others => '0');
+            if hh_reg = 23 then
+              hh_reg <= (others => '0');
+            else
+              hh_reg <= hh_reg + 1;
+            end if;
+          else
+            mm_reg <= mm_reg + 1;
+          end if;
+        else
+          ss_reg <= ss_reg + 1;
+        end if;
+      end if;
+    end if;
+  end process;
+
+  --------------------------------------------------------------------
+  -- Main FSM for mode and user interaction
+  --------------------------------------------------------------------
+  process(clk, rst)
+  begin
+    if rst = '1' then
+      state <= IDLE;
+      alarm_active_reg <= '0';
+    elsif rising_edge(clk) then
+      case state is
         when IDLE =>
-          if btn_mode = '1' then 
+          if btn_mode = '1' then
             state <= SET_TIME;
+          end if;
 
         when SET_TIME =>
-          if btn_mode = '1' then 
+          if btn_mode = '1' then
             state <= SET_ALARM;
-          if btn_select = '1' then 
+          elsif btn_select = '1' then
             state <= SET_TIME_HH;
-
-        when SET_ALARM =>
-          if btn_mode = '1' then 
-            state <= ACTIVATE_ALARM;
-          if btn_select = '1' then 
-            state <= SET_ALARM_HH;
+          end if;
 
         when SET_TIME_HH =>
-          if btn_select = '1' then 
+          if btn_select = '1' then
             state <= SET_TIME_MM;
-          if btn_inc = '1' then 
-            state <= SET_TIME_HH;
+          elsif btn_inc = '1' then
+            if hh_reg = 23 then
+              hh_reg <= (others => '0');
+            else
+              hh_reg <= hh_reg + 1;
+            end if;
+          end if;
 
         when SET_TIME_MM =>
-          if btn_select = '1' then 
+          if btn_select = '1' then
             state <= SET_TIME_SS;
-          if btn_inc = '1' then 
-            state <= SET_TIME_MM;
+          elsif btn_inc = '1' then
+            if mm_reg = 59 then
+              mm_reg <= (others => '0');
+            else
+              mm_reg <= mm_reg + 1;
+            end if;
+          end if;
 
         when SET_TIME_SS =>
-          if btn_select = '1' then 
+          if btn_select = '1' then
             state <= SET_TIME;
-          if btn_inc = '1' then 
-            state <= SET_TIME_SS;
-          
-        when SET_ALARM_HH =>
-          if btn_mode = '1' then 
-            state <= SET_ALARM_MM;
-          if btn_inc = '1' then 
+          elsif btn_inc = '1' then
+            if ss_reg = 59 then
+              ss_reg <= (others => '0');
+            else
+              ss_reg <= ss_reg + 1;
+            end if;
+          end if;
+
+        when SET_ALARM =>
+          if btn_mode = '1' then
+            state <= ACTIVATE_ALARM;
+          elsif btn_select = '1' then
             state <= SET_ALARM_HH;
+          end if;
+
+        when SET_ALARM_HH =>
+          if btn_mode = '1' then
+            state <= SET_ALARM_MM;
+          end if;
 
         when SET_ALARM_MM =>
-          if btn_mode = '1' then 
+          if btn_mode = '1' then
             state <= SET_ALARM;
-          if btn_inc = '1' then 
-            state <= SET_ALARM_MM;
+          end if;
 
         when ACTIVATE_ALARM =>
-          if btn_mode = '1' then 
+          if btn_mode = '1' then
             state <= IDLE;
+          elsif btn_inc = '1' then
+            alarm_active_reg <= not alarm_active_reg;
+          end if;
 
+        when others =>
+          state <= IDLE;
       end case;
     end if;
   end process;
 
-end architecture top_watch_architecture;
+end architecture;
+
 
 
